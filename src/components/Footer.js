da02aac03dd87752506f83809ab9730de60f4878
Vishal <ivishalsah01oct@gmail.com>
2021-06-17 21:30:55 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import {memo} from 'react';
+// eslint-disable-next-line prettier/prettier
+import { GitHub, Database,} from 'react-feather';
+import {useTranslation} from 'react-i18next';
+
+function Footer() {
+  const {t} = useTranslation();
+
+  return (
+    <footer>
+      <div className="link">
+        <a
+          href="https://github.com/Vishal-sah01/CovidTracker2021React.git"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          covid19india
+        </a>
+      </div>
+
+      <h5>{t('We stand with everyone fighting on the frontlines')}</h5>
+
+      <div className="links">
+        <a
+          href="https://github.com/Vishal-sah01/CovidTracker2021React.git"
+          className="github"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <GitHub />
+        </a>
+
+        <a
+          className="api"
+          href="https://api.covid19india.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Database />
+        </a>
+      </div>
+    </footer>
+  );
+}
+
+export default memo(Footer);
